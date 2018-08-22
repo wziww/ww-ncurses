664995562aa066655dace5e6e20d050fadfb010e
maoshuai <shuai.mao@upai.com>
2018-08-22 19:40:08 +0800

    init

--- test.js
+++ test.js
@@ -2,14 +2,31 @@ const cluster = require('cluster');
 const child_process = require('child_process');
 const ncurses = require('./index');
 const {
-    CLI
+    CLI,
+    parseCh
 } = require('./index')
 ncurses.initscr();
+ncurses.clear();
+ncurses.noecho();
+ncurses.hide_cur(0);
+const READ_SNACK = 1;
+ncurses.start_color();
+ncurses.use_default_colors();
+ncurses.init_pair(READ_SNACK, ncurses.COLOR_RED, ncurses.COLOR_WHITE);
 let s = new CLI({
-    height: ncurses.row() - 4
+    height: ncurses.row() - 4,
+    width: 20,
+    left: 0
 });
-s.setTips(['', '1. -q quit', '2. I\'m No.2']).draw();
-
+s.draw();
+let s2 = new CLI({
+    height: ncurses.row() - 4,
+    width: 20,
+    left: 29
+});
+s2.setTips().draw();
+s2.select({
+}).draw();
 
 if (cluster.isMaster) {
     const quit = () => {
@@ -25,7 +42,16 @@ if (cluster.isMaster) {
     cluster.on('message', (worker, cmd, handle) => {
         if (cmd == 'q') {
             return quit();
-        } else {}
+        } else {
+            if(cmd===ncurses.KEY_RIGHT){
+                s.unSelect().draw();
+                s2.select({color: READ_SNACK}).draw();
+            }
+            if(cmd===ncurses.KEY_LEFT){
+                s2.unSelect().draw();
+                s.select({color: READ_SNACK}).draw();
+            }
+        }
     });
 } else {
     while (true) {
